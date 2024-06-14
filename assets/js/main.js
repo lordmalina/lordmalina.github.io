7a95da2b213a139aee78e040e26d2e2c2670881c
lordmalina <147543660+lordmalina@users.noreply.github.com>
2024-06-14 13:16:43 +0200

    pridani videa a uprava slideru

--- assets/js/main.js
+++ assets/js/main.js
@@ -232,20 +232,21 @@
   /**
    * Portfolio details slider
    */
-  new Swiper('.portfolio-details-slider', {
+  var swiper = new Swiper('.portfolio-details-slider', {
     speed: 400,
     loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
       clickable: true
-    }
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   });
-
+  
+  
   /**
    * Initiate Pure Counter 
    */
